2820bc5698aceb7de79c9d0ba920eff016124a7a
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2022-11-12 16:04:43 -0500

    get.py complete

--- pipeline/scripts/get.py
+++ pipeline/scripts/get.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
@@ -28,4 +35,59 @@ product = None
 
 
 # %%
-# your code here...
+#import some necessary librairies
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# %matplotlib inline
+import matplotlib.pyplot as plt  # Matlab-style plotting
+import seaborn as sns
+color = sns.color_palette()
+sns.set_style('darkgrid')
+import warnings
+def ignore_warn(*args, **kwargs):
+    pass
+warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
+
+
+from scipy import stats
+from scipy.stats import norm, skew #for some statistics
+
+
+pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x)) #Limiting floats output to 3 decimal points
+
+
+# %%
+#Now let's import and put the train and test datasets in  pandas dataframe
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+# %%
+train.head(5)
+
+
+# %%
+test.head(5)
+
+# %%
+#check the numbers of samples and features
+print("The train data size before dropping Id feature is : {} ".format(train.shape))
+print("The test data size before dropping Id feature is : {} ".format(test.shape))
+
+#Save the 'Id' column
+train_ID = train['Id']
+test_ID = test['Id']
+
+#Now drop the  'Id' colum since it's unnecessary for  the prediction process.
+train.drop("Id", axis = 1, inplace = True)
+test.drop("Id", axis = 1, inplace = True)
+
+#check again the data size after dropping the 'Id' variable
+print("\nThe train data size after dropping Id feature is : {} ".format(train.shape)) 
+print("The test data size after dropping Id feature is : {} ".format(test.shape))
+
+train = pd.read_csv('train.csv')
+train.to_csv(product['data'], index=False)
+test = pd.read_csv('test.csv')
+test.to_csv(product['data'], index=False)
